37169be66d79e39822f1a6326381387033b7b783
Asianscode <me2020boy@gmail.com>
2024-11-20 06:48:20 +0500

    Update __init__.py
    the addon was missing on metadata so it was not being correctly installed and utilized my blender not even showing up in the addon

--- scripts/modules/goo/__init__.py
+++ scripts/modules/goo/__init__.py
@@ -7,6 +7,17 @@ from .force import create_force
 from .division import *
 from .handler import *
 
-__version__ = "1.0.0"
-__author__ = 'Antoine A. Ruzette, Charles Dai, Sean Megason'
+bl_info = {
+    "name": "Goo Add-on",
+    "author": "Antoine A. Ruzette, Charles Dai, Sean Megason",
+    "version": (1, 0, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > Toolshelf > Goo Panel",
+    "description": "A tool for simulating cellular interactions and dynamics.",
+    "warning": "",
+    "wiki_url": "https://github.com/smegason/Goo",
+    "tracker_url": "https://github.com/smegason/Goo/issues",
+    "category": "Simulation",
+}
+"""solved the addon not showing problem"""
 __credits__ = 'Harvard Medical School'
